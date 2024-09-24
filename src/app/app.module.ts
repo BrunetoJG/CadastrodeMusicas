0fadb823a017f0374b65912209bf085cce7521b0
BrunetoJG <brunojuni@yahoo.com.br>
2024-09-24 17:55:44 -0300

    closes #2 - Integracao com o json server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MusicTableComponent } from './music-table/music-table.component';
-import { TypesComponent } from './types/types.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MusicTableComponent,
-    TypesComponent
+    
   ],
   imports: [
     BrowserModule,
